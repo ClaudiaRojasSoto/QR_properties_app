f0a2a21724e8f1049fa7cd2708ac66160b59d96b
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-02-07 14:15:04 -0300

    Config the configuration on environment and production to use the right host and add the route to properties

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
+  # Change the miapp.com to the rigth url later.
+  Rails.application.routes.default_url_options[:host] = 'miaplicacion.com'
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
